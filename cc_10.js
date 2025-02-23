7a53b9dd88134e090eed0b78ba33ff1cae511765
Kamali Hicks <kamalikh3@gmail.com>
2025-02-23 18:32:29 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -14,3 +14,28 @@ class Product { // Create class
         this.stock -= quantity;
     };
 }
+
+const prod1 = new Product("Laptop", 101, 1200, 10); // Expected output: Product: Laptop, ID: 101, Price: $1200, Stock: 10
+console.log(prod1.getDetails()); 
+prod1.updateStock(3); // Update stock by -3
+console.log(prod1.getDetails()); // Expected output: Product: Laptop, ID: 101, Price: $1200, Stock: 7
+
+// Task 2 Creating an Order Class
+
+class Order { // Create Order class // Properties for order class
+    constructor(orderID, product, quantity) {
+        this.orderID = orderID;
+        this.product = product;
+        this.quantity = quantity
+        this.totalPrice = product.price * quantity; // Calculate total price
+        this.product.updateStock(quantity); // Update stock quantity when creating a order 
+    }
+    getOrderDetails() {
+        return `Order ID: ${this.orderID}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.totalPrice}` // Method to return string of details 
+    }
+}
+
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails()); // Expected output: Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400
+console.log(prod1.getDetails()); // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5 
+
